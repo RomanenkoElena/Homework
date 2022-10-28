28f14c373e8876cbdcab34eb61d3a78edf55a8a6
Elena Romanenko <murenec_elena@mail.ru>
2022-10-28 15:36:36 +0600

    решена задача 6

---
+++ ex06/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является число чётным (делится ли оно на два без остатка).
+
+int currentNumber = new Random().Next(1, 100);
+Console.WriteLine($"Вводим число {currentNumber}");
+if (currentNumber % 2 == 0)
+{
+    Console.WriteLine($"Число {currentNumber} является чётным");
+}
+else
+{
+    Console.WriteLine($"Число {currentNumber} не является чётным");
+}
