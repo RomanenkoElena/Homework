cf7dc833440dd3c03c6729e01f47177c3be920de
Elena Romanenko <murenec_elena@mail.ru>
2022-10-28 14:39:06 +0600

    решена задача 4

---
+++ ex04/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает три числа и выдаёт максимальное из этих чисел
+
+int numberA = new Random().Next(1, 100);
+int numberB = new Random().Next(1, 100);
+int numberC = new Random().Next(1, 100);
+Console.WriteLine($"Первое число {numberA}");
+Console.WriteLine($"Второе число {numberB}");
+Console.WriteLine($"Третье число {numberC}");
+int max_number = numberA;
+if (numberB > numberA)
+{
+    max_number = numberB;
+}
+if (numberC > max_number)
+{
+    max_number = numberC;
+}
+Console.WriteLine($"Максимальное число {max_number}");
