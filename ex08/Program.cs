b33e9c9d548bdb72e0ce4cfa890f8015f592e192
Elena Romanenko <murenec_elena@mail.ru>
2022-10-28 16:53:00 +0600

    решена задача 8

---
+++ ex08/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число(N),а на выходе показывает все чётные числа от 1 до N
+
+int numberN = new Random().Next(1, 100);
+Console.WriteLine($"Вводим число {numberN}");
+int startNumber = 2;
+int endNumber = numberN;
+Console.WriteLine($"Чётные числа от 1 до {numberN}:");
+while (startNumber <= numberN)
+{
+    Console.WriteLine(startNumber);
+    startNumber += 2;
+}
