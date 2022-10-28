f25c53f47afd5fd91916d17fa8bb4deff0e95805
Elena Romanenko <murenec_elena@mail.ru>
2022-10-28 15:09:26 +0600

    решена задача 2

---
+++ ex02/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int numberA = new Random().Next(1, 100);
+int numberB = new Random().Next(1, 100);
+Console.WriteLine($"Первое число {numberA}");
+Console.WriteLine($"Второе число {numberB}");
+if (numberB > numberA)
+{
+   Console.WriteLine($"Число {numberB} большее, число {numberA} меньшее"); 
+}
+else
+{
+    Console.WriteLine($"Число {numberA} большее, число {numberB} меньшее");
+}
